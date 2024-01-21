cd0057816eab26f1adae38c04dd95f3897bf4f0e
Sergey Solodovnyk <xxxsergxxx1990@gmail.com>
2024-01-21 21:04:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter);
+app.use("/routes/contactsRouter", contactsRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
